89380501af49719cd93f3755528bba33ddaefd10
Zonylaw <sunny_law@hotmail.com>
2023-07-09 20:41:27 +0100

    set up with ejs and use include

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
-const exphbs = require("express-handlebars");
+// const exphbs = require("express-handlebars");
 
-const hbs = exphbs.create({
-  layoutsDir: "views/layouts/",
-  defaultLayout: "main-layout",
-  extname: ".hbs",
-});
+// const hbs = exphbs.create({
+//   layoutsDir: "views/layouts/",
+//   defaultLayout: "main-layout",
+//   extname: ".hbs",
+// });
 
 const app = express();
 
-app.engine("hbs", hbs.engine);
+// app.engine("hbs", 'hbs.engine');
 
 // by default in pugs it looks into the views folder for the html but here we define the views, views just for illustartion
-app.set("view engine", "hbs");
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminData = require("./routes/admin");
@@ -27,7 +27,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page not found!" });
+  res.status(404).render("404", { pageTitle: "Page not found!", path: "" });
 });
 
 app.listen(3000);
