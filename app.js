b3576bc0c02bfb1892d7f7e8d1f1637310fa80df
Zonylaw <sunny_law@hotmail.com>
2023-07-09 05:07:24 +0100

    update the 404 and add-product as pug files

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+// by default in pugs it looks into the views folder for the html but here we define the views, views just for illustartion
 app.set("view engine", "pug");
 app.set("views", "views");
 
@@ -17,7 +18,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404");
 });
 
 app.listen(3000);
