a4a46f58e2df35f64087780eee41a9a3c9045812
Zonylaw <sunny_law@hotmail.com>
2023-07-23 16:54:02 +0100

    install sequelize structure in app

--- app.js
+++ app.js
@@ -2,34 +2,16 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
-// const exphbs = require("express-handlebars");
-
-// const hbs = exphbs.create({
-//   layoutsDir: "views/layouts/",
-//   defaultLayout: "main-layout",
-//   extname: ".hbs",
-// });
-
 const app = express();
 
 const errorController = require("./controllers/error");
-const db = require("./util/database");
-// app.engine("hbs", 'hbs.engine');
+const sequelize = require("./util/database");
 
-// by default in pugs it looks into the views folder for the html but here we define the views, views just for illustartion
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-
-// db.execute("SELECT * FROM products")
-//   .then((result) => {
-//     console.log(result[0], result[1]);
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -38,5 +20,14 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+
+sequelize
+  .sync()
+  .then((results) => {
+    console.log(results);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000);
