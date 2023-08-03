bd7884edc7707e0b103e199b0e7fc2e96c04ce49
Zonylaw <sunny_law@hotmail.com>
2023-08-03 17:15:41 +0100

    install mongodb and added login details

--- util/database.js
+++ util/database.js
@@ -1,14 +1,31 @@
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  "node-complete",
-  "root",
-  process.env.DB_PASSWORD,
-  {
-    dialect: "mysql",
-    host: "localhost",
-  }
-);
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    `mongodb+srv://Sunny:${process.env.DB_PASSWORD}@cluster0.ars0ie4.mongodb.net/?retryWrites=true&w=majority`
+  )
+    .then((client) => {
+      console.log("connected");
+      console.log(client);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
-module.exports = sequelize;
+module.exports = mongoConnect;
+// const Sequelize = require("sequelize");
+
+// const sequelize = new Sequelize(
+//   "node-complete",
+//   "root",
+//   process.env.DB_PASSWORD,
+//   {
+//     dialect: "mysql",
+//     host: "localhost",
+//   }
+// );
+
+// module.exports = sequelize;
