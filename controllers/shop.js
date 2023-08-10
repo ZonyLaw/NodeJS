671bd697eb39ec22f2a5363bf6478b78a0ca1499
Zonylaw <sunny_law@hotmail.com>
2023-08-10 23:40:36 +0100

    update detail page with mongodb object

--- controllers/shop.js
+++ controllers/shop.js
@@ -16,7 +16,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
+  Product.findById(prodId)
     .then((product) => {
       // let title = product.title;
       res.render("shop/product-detail", {
