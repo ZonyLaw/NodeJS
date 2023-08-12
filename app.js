78debc93bb883c54926efd2b2f78e4e1b1592059
Zonylaw <sunny_law@hotmail.com>
2023-08-12 23:14:28 +0100

    change the user id

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  User.findById("64d722990ce9e5c42b50d15d")
+  User.findById("64d8027ae3f557b92fc23f9a")
     .then((user) => {
       req.user = new User(user.name, user.email, user.cart, user._id);
       next();
