bb61c71cf67a4e69dfcf24bfe393259fef4291d3
Zonylaw <sunny_law@hotmail.com>
2023-08-12 23:32:22 +0100

    change user id 2nd time2
    2
    2
    2'

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  User.findById("64d8027ae3f557b92fc23f9a")
+  User.findById("64d8076ee3f557b92fc23f9b")
     .then((user) => {
       req.user = new User(user.name, user.email, user.cart, user._id);
       next();
