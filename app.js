f912f07ce151a1c9fb9cd7c5186e849dd038575f
Zonylaw <sunny_law@hotmail.com>
2023-08-15 22:35:06 +0100

    add mongoose

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
-const path = require("path");
-const express = require("express");
-const bodyParser = require("body-parser");
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const errorController = require("./controllers/error");
-const mongoConnect = require("./util/database").mongoConnect;
-const User = require("./models/user");
+const mongoose = require('mongoose');
+
+const errorController = require('./controllers/error');
+const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
 const app = express();
 
 // by default in pugs it looks into the views folder for the html but here we define the views, views just for illustartion
-app.set("view engine", "ejs");
-app.set("views", "views");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById("64d87235f20e338e2357f820")
+  User.findById('64d87235f20e338e2357f820')
     .then((user) => {
       req.user = new User(user.name, user.email, user.cart, user._id);
       next();
@@ -27,7 +29,7 @@ app.use((req, res, next) => {
     .catch((err) => console.log(err));
 });
 
-app.use("/admin", adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
@@ -35,3 +37,7 @@ app.use(errorController.get404);
 mongoConnect(() => {
   app.listen(3000);
 });
+
+mongoose.connect(
+  'mongodb+srv://Sunny:<password>@cluster0.ars0ie4.mongodb.net/?retryWrites=true&w=majority'
+);
