22c05c350a814c5ed66544d69d13f03356bb890f
Zonylaw <sunny_law@hotmail.com>
2023-09-02 22:44:03 +0100

    add flash in the signup page

--- controllers/auth.js
+++ controllers/auth.js
@@ -20,9 +20,18 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.getSignup = (req, res, next) => {
+  let message = req.flash('error');
+  console.log(message);
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup',
+    errorMessage: message,
   });
 };
 
@@ -49,6 +58,7 @@ exports.postLogin = (req, res, next) => {
               res.redirect('/');
             });
           }
+          req.flash('error', 'Invalid email or password.');
           res.redirect('/login');
         })
         .catch((err) => {
@@ -69,6 +79,7 @@ exports.postSignup = (req, res, next) => {
   User.findOne({ email: email })
     .then((userDoc) => {
       if (userDoc) {
+        req.flash('error', 'Email already exist!');
         return res.redirect('/signup');
       }
       return bcrypt
