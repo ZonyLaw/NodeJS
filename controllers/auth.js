8e06bb0beb8f52c04d4ea2260b5546d185fc6fb9
Zonylaw <sunny_law@hotmail.com>
2023-09-23 00:23:32 +0100

    add code to check user exist

--- controllers/auth.js
+++ controllers/auth.js
@@ -75,7 +75,6 @@ exports.postLogin = (req, res, next) => {
 exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  const confirmPassword = req.body.confirmPassword;
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
@@ -87,27 +86,19 @@ exports.postSignup = (req, res, next) => {
     });
   }
 
-  User.findOne({ email: email })
-    .then((userDoc) => {
-      if (userDoc) {
-        req.flash('error', 'Email already exist!');
-        return res.redirect('/signup');
-      }
-      return bcrypt
-        .hash(password, 12)
-        .then((hashedPassword) => {
-          const user = new User({
-            email: email,
-            password: hashedPassword,
-            cart: { itmes: [] },
-          });
-          return user.save();
-        })
-        .then((result) => {
-          res.redirect('/login');
-        });
+  bcrypt
+    .hash(password, 12)
+    .then((hashedPassword) => {
+      const user = new User({
+        email: email,
+        password: hashedPassword,
+        cart: { itmes: [] },
+      });
+      return user.save();
     })
-
+    .then((result) => {
+      res.redirect('/login');
+    })
     .catch((err) => {
       console.log(err);
     });
